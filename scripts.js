1781d438a0f356cf8f114406a73abe67a2d73896
WaltVenditi <waltvenditti@gmail.com>
2021-11-05 18:58:59 -0400

    started on new color pattern

--- scripts.js
+++ scripts.js
@@ -25,7 +25,21 @@ function addHover() {
     squares = document.querySelectorAll('.square');
     squares.forEach((sqr) => {
         sqr.addEventListener('mouseover', () => {
-            sqr.style['background-color'] = 'black';
+            style = window.getComputedStyle(sqr);
+            currentColor = style.getPropertyValue('background-color');
+
+            if (currentColor == 'rgb(255, 255, 255)') {
+            const colors = genRandomColor();
+            sqr.style['background-color'] = `rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, 0.1`;
+            } else {
+                alpha = currentColor.slice(-2, -1);
+                console.log(alpha);
+                alpha++;
+                newColor = currentColor.slice(0, -2);
+                newColor = newColor + alpha + ')';
+                console.log(newColor);
+                sqr.style['background-color'] = newColor;
+            };
         });
     });
 }
@@ -35,7 +49,30 @@ function clearGrid() {
     rows.forEach((row) => {
         row.remove();
     });
-};
+}
+
+function genRandomColor() {
+    let red = Math.floor((Math.random() * 255) +1);
+    let grn = Math.floor((Math.random() * 255) +1);
+    let blu = Math.floor((Math.random() * 255) +1);
+    const colors = [red, grn, blu];
+    return colors;
+}
+
+/*
+function testColor () {
+    x = 1;
+    while (x) {
+        let colors = genRandomColor();
+        for (i=0; i<3; i++) {
+            if (colors[i] === 255) {
+                console.log(colors);
+                x = 0;
+            };
+        };
+    };
+}
+*/
 
 btn = document.querySelector('button');
 btn.addEventListener('click', () => {
